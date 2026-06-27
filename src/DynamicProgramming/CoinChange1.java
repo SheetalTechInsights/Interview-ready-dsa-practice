30079774065c64bd615ffe5f48d64900fbdca47d
Sheetal Patidargit config --global user.email sheetalpatidar969@gmail.com <yourgithubemail@example.com>
2026-06-27 19:15:34 +0530

    Day-6 of Blind-75 LeetCode(DP)

---
+++ src/DynamicProgramming/CoinChange1.java
@@ -0,0 +1,29 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class CoinChange1 {
+    public  int change(int[]coins,int amount){
+       int []dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int coin:coins){
+                if(i-coin>=0){
+                    dp[i]= Math.min(dp[i],dp[i-coin]+1);
+                }
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
+
+    }
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+
+        CoinChange1 obj = new CoinChange1();
+        int ans = obj.change(coins, amount);
+
+        System.out.println(ans);
+    }
+}
